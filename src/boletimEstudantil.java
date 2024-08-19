fcec261fa69de815583be552ef3267a3a2d2df6a
Lussati Santos <lussatisantos10@gmail.com>
2024-08-19 09:09:22 +0100

    exercicio boletim estudantil

---
+++ src/boletimEstudantil.java
@@ -0,0 +1,14 @@
+public class boletimEstudantil {
+    public static void main (String [] args){
+        int mediaFinal = 5;
+        
+        if(mediaFinal < 6){
+            System.out.println("Reprovado!");
+        } else if (mediaFinal == 6){
+            System.out.println("Recurso!");
+        } else {
+            System.out.println("Aprovado!");
+        }
+
+    }
+}
