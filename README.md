a23fe6968b50134cbe86704995e2f36ebd5adc26
Lussati Santos <lussatisantos10@gmail.com>
2024-08-19 09:02:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-anatomy-classes
+ 
---
+++ src/Apps.java
@@ -0,0 +1,12 @@
+public class Apps {
+    public static void main(String [] args) {
+       // System.out.println("Hello World!");
+       //  System.out.print ("Ola Mundo!");
+
+       /* */
+    }
+
+    public static String nomeCompleto (String nome, String sobreNome){
+        return nome.concat(" ").concat(sobreNome);
+    }
+}
