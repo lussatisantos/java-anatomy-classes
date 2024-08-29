4e54d8700aa8802209850978d1e261eb091fd07f
Lussati Santos <lussatisantos10@gmail.com>
2024-08-29 16:21:17 +0100

    add context

--- src/edu/lussatisantos/thirdweek/Excpetion.java
+++ src/edu/lussatisantos/thirdweek/Excpetion.java
@@ -1,5 +1,33 @@
 package edu.lussatisantos.thirdweek;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Excpetion {
-    
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.UK);
+
+            int agencyNumber;
+            int accountNumber;
+            String clientName;
+            double balance;
+
+            System.out.print("Enter agency number: ");
+            agencyNumber = scanner.nextInt();
+            System.out.print("Enter account number: ");
+            accountNumber = scanner.nextInt();
+            System.out.print("Enter client name: ");
+            clientName = scanner.next();
+            System.out.print("Enter deposit amount: ");
+            balance = scanner.nextDouble();
+
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Agency Number: " + agencyNumber);
+            System.out.println("Client Name: " + clientName);
+            System.out.println("Current Balance: " + balance);
+
+            System.out.print("Hello " + clientName + " welcome in our bank, your balance " + balance + " is now available");
+
+            scanner.close();
+        }
 }
